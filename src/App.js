e57378a2930c6c14e18caac4e7577ddf5441ef22
NehalHazem <nehalhazem.nh@gmail.com>
2026-03-18 03:20:45 +0200

    update website

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './components/layout';
 import LayoutAR from './components/ar/layoutAR';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/weddingInvitation" element={<Layout />} />
         <Route path="/weddingInvitation/ar" element={<LayoutAR />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
